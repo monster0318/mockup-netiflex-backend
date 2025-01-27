4836cda3d187610e43af966dfd3fe3445cc9436d
Ibrahima SOURABIE <ge65yub@mytum.de>
2025-01-27 19:39:13 +0100

    components integrated

--- videoflix_frontend/src/app/app.component.ts
+++ videoflix_frontend/src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
-import { StartpageComponent } from './components/startpage/startpage.component';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    FooterComponent,
-    NavBarComponent,
-    LogInComponent,
-    ForgotPasswordComponent,
-    ResetPasswordComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- videoflix_frontend/src/app/components/log-in/log-in.component.ts
+++ videoflix_frontend/src/app/components/log-in/log-in.component.ts
@@ -3,11 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { NavBarComponent } from '../../shared/nav-bar/nav-bar.component';
 import { WrapperComponent } from '../../shared/wrapper/wrapper.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [FormsModule, FooterComponent, NavBarComponent, WrapperComponent],
+  imports: [
+    FormsModule,
+    FooterComponent,
+    NavBarComponent,
+    WrapperComponent,
+    RouterLink,
+  ],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss',
 })
