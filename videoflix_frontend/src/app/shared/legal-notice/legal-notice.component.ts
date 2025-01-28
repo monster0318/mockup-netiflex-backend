6a4a0b0bc115d95608e8108f1c9bb63a0a9d0b93
Ibrahima SOURABIE <ge65yub@mytum.de>
2025-01-28 18:58:45 +0100

    Adding legal notice

--- videoflix_frontend/src/app/shared/legal-notice/legal-notice.component.ts
+++ videoflix_frontend/src/app/shared/legal-notice/legal-notice.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, RouterLink],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss'
+  styleUrl: './legal-notice.component.scss',
 })
-export class LegalNoticeComponent {
-
-}
+export class LegalNoticeComponent {}
