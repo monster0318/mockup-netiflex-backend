5d7b2ebef4fef55fa2a8abefbd1972d9bb9bed74
Ibrahima SOURABIE <ge65yub@mytum.de>
2025-02-22 13:53:01 +0100

    Switching from generics views to viewsets

--- videoflix_backend/videoflix_app/api/serializers.py
+++ videoflix_backend/videoflix_app/api/serializers.py
@@ -7,7 +7,7 @@ class VideoSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=150)
     author = serializers.CharField(max_length=150)
     description = serializers.CharField(max_length=1000,required=False)
-    created_by = UserSerializer(read_only=True)
+    uploaded_by = UserSerializer(read_only=True)
     class Meta:
         model = Video
         fields = "__all__"
--- videoflix_backend/videoflix_app/api/urls.py
+++ videoflix_backend/videoflix_app/api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from videoflix_app.api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("videos", views.VideoViewSet)
 
 urlpatterns = [
-    path('videos/',views.VideoListView.as_view(), name='video-list'),
-    path('video/<int:pk>/',views.SingleVideoView.as_view(), name='video-detail'),
+    path("",include(router.urls)),
+    # path('videos/',views.VideoListView.as_view(), name='video-list'),
+    # path('video/<int:pk>/',views.SingleVideoView.as_view(), name='video-detail'),
 ]
