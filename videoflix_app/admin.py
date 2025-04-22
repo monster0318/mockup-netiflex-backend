e169321dae6febd4d72b6c15a49de8a791d57e3d
monster0318 <loyalman318@gmail.com>
2025-04-22 06:50:06 -0700

    new feature : video list get api
    Co-authored-by: brightsnow318 <tinneyrodney128@gmail.com>

--- videoflix_app/admin.py
+++ videoflix_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from import_export import resources
-from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import Video
+from import_export import resources
+
+
+
+
+
+
 
 
 class VideoResource(resources.ModelResource):
